5627981b908d0dcbd52322c1ec2ea8f9ea64f171
Austin McKinley <bearontheroof@gmail.com>
2015-03-09 18:41:41 -0700

    switch to reasonable logging, fix hardcoded paths, start fleshing out deck support

--- heathskin/deck.py
+++ heathskin/deck.py
@@ -2,6 +2,9 @@ from collections import defaultdict
 import copy
 import json
 import operator
+import logging
+
+from heathskin import utils
 
 CARD_DATA_PATH = "data/AllSets.json"
 
@@ -13,9 +16,11 @@ def deck_from_file(path):
 
 class Deck(object):
     def __init__(self, card_list):
+        self.logger = logging.getLogger()
         if len(card_list) != 30:
             raise Exception("you must have exactly 30 cards; found {}".format(len(card_list)))
 
+        self.read_card_data()
         self.card_list = card_list
 
         valid_cards = self.get_all_valid_names()
@@ -23,13 +28,72 @@ class Deck(object):
             if c not in valid_cards:
                 raise Exception("card is not valid: {}".format(c))
 
-    def get_all_valid_names(self):
-        with open(CARD_DATA_PATH) as f:
-            card_data = json.load(f)
 
+        #self.get_real_set_names()
+        #print self.card_data.keys()
+        self.get_taunters()
+
+    def read_card_data(self):
+        with open(CARD_DATA_PATH) as f:
+            raw = json.load(f)
+            self.card_data = {n: d for n, d in raw.items() if n in self.get_real_set_names()}
+
+    def get_real_set_names(self):
+        names = [
+            "Basic",
+            "Classic",
+            #"Credits",
+            "Curse of Naxxramas",
+            #"Debug",
+            "Goblins vs Gnomes",
+            #"Missions",
+            "Promotion",
+            "Reward"]
+            #"System"]
+        return names
+
+        print len(self.card_data.items())
+
+        for n in names:
+            card_set = self.card_data[n]
+            print n, len(card_set)
+            for c in card_set:
+                print c['name']
+
+            print
+
+    def get_taunters(self):
+        name_check = defaultdict(list)
+        taunters = list()
+        for set_name, card_set in self.card_data.items():
+            for c in card_set:
+                name_check[c['name']].append(c)
+
+
+        for name, cards in name_check.items():
+            if len(cards) == 1:
+                continue
+
+            differ = utils.MultiDictDiffer(cards)
+            print "[{}] {}".format(name, len(cards))
+            differ.get_diff()
+            print
+            # for k in changed:
+            #     print "{} [{}] - [{}]".format(k, cards[0][k], cards[1][k])
+
+            # print
+
+                #[c['id'] for c in cards]
+
+        #         if 'mechanics' in c and "Taunt" in c['mechanics']:
+        #             taunters.append(c)
+
+        # for c in taunters:
+        #     print c['name'], c['id']
+
+    def get_all_valid_names(self):
         valid_names = set()
-        for set_name, card_set in card_data.items():
-            print set_name
+        for set_name, card_set in self.card_data.items():
             for c in card_set:
                 valid_names.add(c["name"])
 
@@ -37,7 +101,6 @@ class Deck(object):
 
     def consume_card_by_name(self, card_name):
         if card_name in self.in_deck:
-            print "consuming card", card_name
             self.in_deck.remove(card_name)
             self.consumed.append(card_name)
         else:
@@ -64,7 +127,7 @@ class Deck(object):
             count_str = ""
             if counts[c] != 1:
                 count_str = " (2x)"
-            print (c + count_str).ljust(30), ":", "%.2f" % (prob * 100.0) + "%"    
+            print (c + count_str).ljust(30), ":", "%.2f" % (prob * 100.0) + "%"
 
     def get_card_counts(self, cards):
         count_dict = defaultdict(int)
--- heathskin/utils.py
+++ heathskin/utils.py
@@ -1,3 +1,5 @@
+from tabulate import tabulate
+
 def params_to_dict(param_str):
     splits = param_str.split(" ")
     data = {}
@@ -17,3 +19,56 @@ def params_to_dict(param_str):
 
     data[prev_key] = prev_val
     return data
+
+class MultiDictDiffer(object):
+    def __init__(self, dicts):
+        self.ds = dicts
+
+    def get_diff(self):
+        all_keys = set()
+        for d in self.ds:
+            for k in d.keys():
+                all_keys.add(k)
+
+        diff_table = []
+        for k in all_keys:
+            values = list()
+            for d in self.ds:
+                values.append(d.get(k, "<missing>"))
+
+            first_val = values[0]
+            found_diff = False
+            for v in values[1:]:
+                if v != first_val:
+                    found_diff = True
+                    break
+
+            if found_diff:
+                values.insert(0, k)
+                diff_table.append(values)
+
+        print tabulate(diff_table)
+
+
+
+
+class DictDiffer(object):
+    """
+    Calculate the difference between two dictionaries as:
+    (1) items added
+    (2) items removed
+    (3) keys same in both but changed values
+    (4) keys same in both and unchanged values
+    """
+    def __init__(self, current_dict, past_dict):
+        self.current_dict, self.past_dict = current_dict, past_dict
+        self.set_current, self.set_past = set(current_dict.keys()), set(past_dict.keys())
+        self.intersect = self.set_current.intersection(self.set_past)
+    def added(self):
+        return self.set_current - self.intersect
+    def removed(self):
+        return self.set_past - self.intersect
+    def changed(self):
+        return set(o for o in self.intersect if self.past_dict[o] != self.current_dict[o])
+    def unchanged(self):
+        return set(o for o in self.intersect if self.past_dict[o] == self.current_dict[o])
