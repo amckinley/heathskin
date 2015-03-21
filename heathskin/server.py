d802bfb8e47beeb3426e74dfc3796a7d29e24751
DoctorAndrew <andrewckoch@gmail.com>
2015-03-21 11:25:07 -0700

    minor refactoring

--- heathskin/server.py
+++ heathskin/server.py
@@ -8,22 +8,6 @@ from heathskin import card_database, game_state, tail_thread
 app = Flask(__name__, static_url_path='', static_folder='../public')
 app.add_url_rule('/', 'root', lambda: app.send_static_file('index.html'))
 
-# @app.route('/random_card.json')
-# def card_handler():
-#     card_db = card_database.CardDatabase.get_database()
-#     hand = random.sample(card_db.search(health=2), 5)
-#     return Response(json.dumps(hand), mimetype='application/json', headers={'Cache-Control': 'no-cache'})
-
-# @app.route('/random_card_image') # dev sandboxing; also broken
-# def imgur():
-#     card_db = card_database.CardDatabase.get_database()
-#     hand = random.sample(card_db.search(health=2), 5)
-#     temp = ''
-#     for card in hand:
-#         temp += (str(make_image_url(id=card['id'])) + ' ')
-#     return temp
-
-
 app_state = {}
 @app.route('/update_state',  methods=['GET', 'POST'])
 def update_state():
@@ -39,8 +23,8 @@ def update_state():
 
     return ""
 
-@app.route("/balls")
-def balls():
+@app.route("/tracker")
+def tacker():
     global app_state
     hand = {}
     print "app_state: ", app_state
@@ -55,38 +39,47 @@ def balls():
                             handsize=len(hand))
     # return "friendly hand: {}".format(app_state['friendly_hand'])
 
-@app.route('/jque')
-def jquerytest():
-    card_db = card_database.CardDatabase.get_database()
-    dev_hand = random.sample(card_db.search(health=2), 20)
-    hand = {}
-    for card in dev_hand:
-        hand[card['name']] = {'name': card['name'],
-                              'imgurl': ('card_images/banners/' + card['id'] + '_banner.png'),
-                              'cost': card['cost']}
-    print "hand: ", hand
-    for card in hand:
-        print "\n" + card
-        print "hand[card]: ", hand[card]
-        print "hand[card]['name']: ", hand[card]['name']
-    return render_template('jquerytest.html',
-                            cards=hand)
 
-@app.route('/images_from_hand')
-def display_hand():
-    card_db = card_database.CardDatabase.get_database()
-    dev_hand = random.sample(card_db.search(health=2), 20)
-    hand = {}
-    for card in dev_hand:
-        hand[card['name']] = {'name': card['name'],
-                              'imgurl': ('card_images/banners/' + card['id'] + '_banner.png')}
-    print "hand: ", hand
-    for card in hand:
-        print "\n" + card
-        print "hand[card]: ", hand[card]
-        print "hand[card]['name']: ", hand[card]['name']
-    return render_template('template.html',
-                            cards=hand)
+# @app.route('/random_card.json')
+# def card_handler():
+#     card_db = card_database.CardDatabase.get_database()
+#     hand = random.sample(card_db.search(health=2), 5)
+#     return Response(json.dumps(hand), mimetype='application/json', headers={'Cache-Control': 'no-cache'})
+
+# @app.route('/images_from_hand')
+# def display_hand():
+#     """Random cards in tracker format, crappier version of random_tracker."""
+#     card_db = card_database.CardDatabase.get_database()
+#     dev_hand = random.sample(card_db.search(health=2), 20)
+#     hand = {}
+#     for card in dev_hand:
+#         hand[card['name']] = {'name': card['name'],
+#                               'imgurl': ('card_images/banners/' + card['id'] + '_banner.png')}
+#     print "hand: ", hand
+#     for card in hand:
+#         print "\n" + card
+#         print "hand[card]: ", hand[card]
+#         print "hand[card]['name']: ", hand[card]['name']
+#     return render_template('template.html',
+#                             cards=hand)
+
+# @app.route('/random_tracker')
+# def random_tracker():
+# """Displays random cards in tracker format for dev templating."""
+#     card_db = card_database.CardDatabase.get_database()
+#     dev_hand = random.sample(card_db.search(health=2), 20)
+#     hand = {}
+#     for card in dev_hand:
+#         hand[card['name']] = {'name': card['name'],
+#                               'imgurl': ('card_images/banners/' + card['id'] + '_banner.png'),
+#                               'cost': card['cost']}
+#     print "hand: ", hand
+#     for card in hand:
+#         print "\n" + card
+#         print "hand[card]: ", hand[card]
+#         print "hand[card]['name']: ", hand[card]['name']
+#     return render_template('jquerytest.html',
+                            # cards=hand)
 
 if __name__ == '__main__':
     app.debug = True
