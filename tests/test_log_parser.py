294b6345a4ef69fa36e2f123289ae9ac5c9f160b
DoctorAndrew <andrewckoch@gmail.com>
2015-05-10 22:45:19 -0700

    adding more unit tests, utils takes player arg

--- tests/test_log_parser.py
+++ tests/test_log_parser.py
@@ -4,9 +4,9 @@ from heathskin import card_database
 from utils import replayer
 
 
-class TestLogParser(unittest2.TestCase):
+class TestSimpleConcede(unittest2.TestCase):
     def setUp(self):
-        self.gs = replayer("tests/logs/simple_concede.log")
+        self.gs = replayer("tests/logs/simple_concede.log", "austin")
         self.card_db = card_database.CardDatabase.get_database()
 
     def test_simple_concede(self):
@@ -23,6 +23,34 @@ class TestLogParser(unittest2.TestCase):
         opposing_health = self.gs.get_opposing_health()
         self.assertEqual(friendly_health, 27)
         self.assertEqual(opposing_health, 26)
+
+        print self.gs.get_friendly_played_cards()
+        print friendly_player.tags.items()
+
+        self.assertEqual(self.gs.get_num_turns(), 3)
+
+        self.assertEqual(self.gs.get_friendly_player_did_act_first(), True)
+        self.gs._create_history()
+
+class TestShortMage(unittest2.TestCase):
+    def setUp(self):
+        self.gs = replayer("tests/logs/shortmage.log", "And0r")
+        self.card_db = card_database.CardDatabase.get_database()
+
+    def test_short_mage(self):
+        friendly_player = self.gs.get_friendly_player()
+        friendly_hero = self.gs.get_friendly_hero()
+
+        opposing_player = self.gs.get_opposing_player()
+        opposing_hero = self.gs.get_opposing_hero()
+
+        self.assertEqual(opposing_hero.name, "Malfurion Stormrage")
+        self.assertEqual(friendly_hero.name, "Jaina Proudmoore")
+
+        friendly_health = self.gs.get_friendly_health()
+        opposing_health = self.gs.get_opposing_health()
+        self.assertEqual(friendly_health, 30)
+        self.assertEqual(opposing_health, 25)
 
         print self.gs.get_friendly_played_cards()
         print friendly_player.tags.items()
